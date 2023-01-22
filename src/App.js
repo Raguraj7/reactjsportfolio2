16729025b4f144f829812abde64e94f947f8368f
unknown <starksplick@gmail.com>
2023-01-22 17:26:44 +0530

    title color change

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ const App = () => {
   return (
     <div className="m-3">
       <div className="flex justify-center items-center">
-        <span onClick={()=>console.log('testing')} className="bg-indigo-800 text-sm px-3 pb-1 text-white text-center rounded-sm">
+        <span onClick={()=>console.log('testing')} className="bg-purple-500 text-sm px-3 pb-1 text-white text-center rounded-sm">
           testing
         </span>
       </div>
